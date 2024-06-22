49a651b428753e858fa7d86e960c699e8db27b51
PeligroLava <Themenprolava@gmail.com>
2024-06-21 21:29:02 -0500

    Fix: He implementado el segundo menu para conectarlo en el merge posteriormente.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { Home2Component } from './home2/home2.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'home2', component: Home2Component}
 ];
 
 @NgModule({
