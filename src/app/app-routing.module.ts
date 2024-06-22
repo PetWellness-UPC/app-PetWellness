e1c24377f2defa537e6414a74678cd6346c4d2fa
PeligroLava <Themenprolava@gmail.com>
2024-06-21 21:42:27 -0500

    Feat: he implementado la primera version del feature VIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreciosComponent } from './precios/precios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/precios', pathMatch: 'full' },
+  { path: 'precios', component: PreciosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreciosComponent } from './precios/precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
